5be27a87bec35de7b9756fdc0ca027a361deeb0d
Jann008 <jann.neururer@edu.tbz.ch>
2025-06-18 16:10:11 +0200

    Start and Stop Script upload

---
+++ stop_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Stop_Script.sh
+# This script is used to stop various Docker containers for a self-hosted cloud project.
+# Ensure the script is run from the correct directory or adjust paths accordingly.
+
+set -e  # Instant Shutdown on error
+
+cd "$(dirname "$0")"  # changes to the directory of the script
+
+cd reverse-proxy/traefik && docker compose down
+cd ../../netdata && docker compose down
+cd ../homer && docker compose down
+cd ../bookstack && docker compose down
+cd ../uptime-kuma && docker compose down
+cd ../filebrowser && docker compose down
